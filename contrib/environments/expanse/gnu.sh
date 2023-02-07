d9d45a4df9bd02707d8453a9ceeea39efe786164
Rob Egan <rsegan@lbl.gov>
2023-02-07 09:00:54 -0800

    removed mpi dependency in upcxx build on Expanse

--- contrib/environments/expanse/gnu.sh
+++ contrib/environments/expanse/gnu.sh
@@ -1,13 +1,9 @@
 module load gcc
-module load openmpi
 module load cmake/3.18.2
+module rm openmpi
 module load slurm
 
-if ! [[ "$PATH" =~ "/home/regan1/install/bin" ]]
-then
-    PATH="$PATH:$HOME/install/bin"
-fi
-export PATH
+# UPCXX built with: BUILD_THREADS=16 ./upcxx-utils/contrib/install_upcxx.sh $SCRATCH/install-upcxx-2022.9 --enable-ibv --with-pmi-version=2 --with-default-net=ibv --enable-pmi --with-ibv-spawner=pmi --with-ibv-physmem-max=1/2 --disable-mpi-compat --with-ibv-max-hcas=3
 
 export PMI_HOME=/cm/shared/apps/slurm/current
 export PMIRUN_CMD="srun --mpi=pmi2 %V -n %N -- %C"
@@ -16,4 +12,5 @@ export GASNET_PHYSMEM_PROBE=0
 export GASNET_PHYSMEM_MAX=1/2
 export GASNET_MAX_SEGSIZE=0.45/H
 
-export MHM2_CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which mpicxx)"
+export PATH=$SCRATCH/install-upcxx-2022.9/bin:$PATH
+export MHM2_CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) "
