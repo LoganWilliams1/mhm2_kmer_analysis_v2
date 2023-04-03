b0f56682944ac3b2e75cbd7175df26e839d729d8
Rob Egan <rsegan@lbl.gov>
2023-04-03 09:03:54 -0700

    fixed default perlmutter environment to include partial vendor workaround for slingshot 11

--- contrib/environments/perlmutter/gnu.sh
+++ contrib/environments/perlmutter/gnu.sh
@@ -5,12 +5,20 @@ module load cudatoolkit
 #module swap PrgEnv-gnu/8.2.0
 module swap gcc/11.2.0
 module remove darshan
-module load craype-hugepages2M
+#module load craype-hugepages2M
+module rm craype-hugepages2M # FIXME when slingshot is fixed
 
 module use /global/common/software/m2878/perlmutter/modulefiles
 module rm upcxx
 # module load upcxx
-module load upcxx/nightly ; export GASNET_OFI_RECEIVE_BUFF_SIZE=recv ; export GASNET_OFI_NUM_RECEIVE_BUFFS=400
+module load upcxx/nightly ; export GASNET_OFI_RECEIVE_BUFF_SIZE=recv ; export GASNET_OFI_NUM_RECEIVE_BUFFS=400 # FIXME when slingshot is fixed
+export FI_OFI_RXM_RX_SIZE=8192 # FIXME when slingshot is fixed
+#export FI_CXI_DEFAULT_CQ_SIZE=13107200 # FIXME when slingshot is fixed
+export FI_CXI_DEFAULT_CQ_SIZE=$((13107200 / 5)) # FIXME when slingshot is fixed
+export FI_MR_CACHE_MONITOR=memhooks # FIXME when slingshot is fixed
+export FI_CXI_RX_MATCH_MODE=software # FIXME when slingshot is fixed
+export FI_CXI_REQ_BUF_MIN_POSTED=10 # FIXME when slingshot is fixed
+export FI_CXI_REQ_BUF_SIZE=25165824 # FIXME when slingshot is fixed
 
 module list
 which cc
