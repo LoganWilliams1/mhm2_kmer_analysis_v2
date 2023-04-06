ddbd40b0f7a1175cfbb0063f6f4c11c0a671d2bf
Steven Hofmeyr <shofmeyr@lbl.gov>
2023-04-06 13:59:10 -0400

    unload hugepages on frontier

--- contrib/environments/frontier/gnu.sh
+++ contrib/environments/frontier/gnu.sh
@@ -19,6 +19,7 @@ export FI_MR_CACHE_MONITOR=memhooks
 export FI_CXI_RX_MATCH_MODE=software
 export FI_CXI_REQ_BUF_MIN_POSTED=10
 export FI_CXI_REQ_BUF_SIZE=25165824
+module rm craype-hugepages2M # FIXME when slingshot is fixed
 
 #module list
 which upcxx
@@ -29,4 +30,3 @@ module load cray-python
 export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which cc) -DCMAKE_CXX_COMPILER=$(which CC) -DENABLE_CUDA=Off -DENABLE_HIP=On"
 export MHM2_BUILD_THREADS=8
 
-# salloc/sbatch with: -A BIF115_crusher --ntasks-per-node=64 
