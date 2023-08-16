ccb9f040ec75ae03a72d4269e4ce4dd1c0812de3
Rob Egan <RSEgan@lbl.gov>
2023-08-16 16:03:59 -0700

    fixed dori env

--- contrib/environments/dori/env.sh
+++ contrib/environments/dori/env.sh
@@ -1,8 +1,5 @@
-which mpirun 2>/dev/null || export PATH=$PATH:/clusterfs/jgi/scratch/gentech/genome_analysis/rsegan/Software/openmpi-4.1.5/bin/
-which cmake 2>/dev/null || export PATH=$PATH:/clusterfs/jgi/scratch/gentech/genome_analysis/rsegan/Software/cmake-3.26.4/bin/
-which upcxx 2>/dev/null || export PATH=$PATH:/clusterfs/jgi/scratch/gentech/genome_analysis/rsegan/Software/upcxx-2023.3.0-ibv/bin
+module use /clusterfs/jgi/groups/gentech/genome_analysis/rnd/rsegan/Software/modulefiles
+module load upcxx
+module load cmake
+module load python
 
-export GASNET_PHYSMEM_MAX='100 GB'
-export GASNET_PHYSMEM_PROBE=NO
-export OMPI_MCA_btl=tcp,vader,self
-export GASNET_USE_ODP=0
