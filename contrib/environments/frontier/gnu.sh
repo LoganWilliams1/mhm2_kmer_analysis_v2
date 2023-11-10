59c62244ad281f07f94b0055f20adb9562e7e6ae
Steven Hofmeyr <shofmeyr@lbl.gov>
2023-11-10 14:25:22 -0500

    changed gnu.sh in frontier envs to best settings that work

--- contrib/environments/frontier/gnu.sh
+++ contrib/environments/frontier/gnu.sh
@@ -7,21 +7,13 @@ module load cmake
 module rm xl
 module load gcc
 module load rocm/5.2.0
-# this causes a remap failure when using GPUs
-#module load craype-hugepages2M
-module use /lustre/orion/csc296/world-shared/frontier/modulefiles
-module rm upcxx
-module load upcxx/2023.3.0
+module load ums ums014 upcxx
+
 export GASNET_OFI_RECEIVE_BUFF_SIZE=recv
-export FI_OFI_RXM_RX_SIZE=8192
-export FI_CXI_DEFAULT_CQ_SIZE=13107200
-export FI_MR_CACHE_MONITOR=memhooks
-export FI_CXI_RX_MATCH_MODE=software
-export FI_CXI_REQ_BUF_MIN_POSTED=10
-export FI_CXI_REQ_BUF_SIZE=25165824
+#export FI_MR_CACHE_MONITOR=memhooks
+#export FI_CXI_RX_MATCH_MODE=software
 module rm craype-hugepages2M # FIXME when slingshot is fixed
 
-#module list
 which upcxx
 upcxx --version
 
